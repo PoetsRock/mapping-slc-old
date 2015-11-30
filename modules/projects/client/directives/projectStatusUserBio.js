d6226a99a263c54e933c97104f3a31d441e8c2f0
PoetsRock <chris@christanseer.com>
2015-11-29 19:10:35 -0700

    files for deployment

---
+++ modules/projects/client/directives/projectStatusUserBio.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('projects').directive('projectStatusUserBio', function() {
+        return {
+          restrict: 'EA',
+          templateUrl: 'modules/projects/client/directives/views/project-status-user-bio.html'
+        };
+    });
