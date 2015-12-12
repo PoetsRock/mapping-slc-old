3c11676eb919e534aa42b403eb90b3c9653a5187
PoetsRock <chris@christanseer.com>
2015-12-11 23:05:55 -0700

    upgraded to mean v0.4.2

--- modules/users/client/config/users.client.config.js
+++ modules/users/client/config/users.client.config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Config HTTP Error Handling
-angular.module('users').config(['$httpProvider',
-  function ($httpProvider) {
+angular.module('users').config(['$httpProvider', 'LightboxProvider',
+  function ($httpProvider, LightboxProvider) {
     // Set the httpProvider "not authorized" interceptor
     $httpProvider.interceptors.push(['$q', '$location', 'Authentication',
       function ($q, $location, Authentication) {
@@ -26,26 +26,62 @@ angular.module('users').config(['$httpProvider',
         };
       }
     ]);
-  }
-]);
 
 
-/**
-    // todo Lightbox
+    /**
+     // todo Lightbox
 
-    // set a custom template
-    LightboxProvider.templateUrl = '/modules/users/client/directives/views/lightbox.html';
+     // set a custom template
+     LightboxProvider.templateUrl = '/modules/users/client/directives/views/lightbox.html';
 
-    // our images array is not in the default format, so we have to write this
-    // custom method
-    LightboxProvider.getImageUrl = function (imageUrl) {
+     // our images array is not in the default format, so we have to write this
+     // custom method
+     LightboxProvider.getImageUrl = function (imageUrl) {
       return imageUrl;
     };
 
-    // set the caption of each image as its text color
-    LightboxProvider.getImageCaption = function (imageUrl) {
+     // set the caption of each image as its text color
+     LightboxProvider.getImageCaption = function (imageUrl) {
       return '#' + imageUrl.match(/00\/(\w+)/)[1];
     };
+
+     // increase the maximum display height of the image
+     LightboxProvider.calculateImageDimensionLimits = function (dimensions) {
+      return {
+        'maxWidth': dimensions.windowWidth >= 768 ? // default
+        dimensions.windowWidth - 92 :
+        dimensions.windowWidth - 52,
+        'maxHeight': 1600                           // custom
+      };
+    };
+
+     // the modal height calculation has to be changed since our custom template is
+     // taller than the default template
+     LightboxProvider.calculateModalDimensions = function (dimensions) {
+      var width = Math.max(400, dimensions.imageDisplayWidth + 32);
+
+      if (width >= dimensions.windowWidth - 20 || dimensions.windowWidth < 768) {
+        width = 'auto';
+      }
+
+      return {
+        'width': width,    // default
+        'height': 'auto'   // custom
+      };
+    };
+
+     **/
+
+
+//.config(function (LightboxProvider) {
+
+      //set a custom template
+    LightboxProvider.templateUrl = '/modules/users/client/views/lightbox.html';
+
+//// set the caption of each image as its text color
+//  LightboxProvider.getImageCaption = function (imageUrl) {
+//    return '#' + imageUrl.match(/00\/(\w+)/)[1];
+//  };
 
     // increase the maximum display height of the image
     LightboxProvider.calculateImageDimensionLimits = function (dimensions) {
@@ -67,47 +103,11 @@ angular.module('users').config(['$httpProvider',
       }
 
       return {
-        'width': width,    // default
-        'height': 'auto'   // custom
+        'width': width,                             // default
+        'height': 'auto'                            // custom
       };
     };
+  }
 
- **/
+]);
 
-
-.config(function (LightboxProvider) {
-
-  //set a custom template
-  LightboxProvider.templateUrl = '/modules/users/client/views/lightbox.html';
-
-//// set the caption of each image as its text color
-//  LightboxProvider.getImageCaption = function (imageUrl) {
-//    return '#' + imageUrl.match(/00\/(\w+)/)[1];
-//  };
-
-  // increase the maximum display height of the image
-  LightboxProvider.calculateImageDimensionLimits = function (dimensions) {
-    return {
-      'maxWidth': dimensions.windowWidth >= 768 ? // default
-      dimensions.windowWidth - 92 :
-      dimensions.windowWidth - 52,
-      'maxHeight': 1600                           // custom
-    };
-  };
-
-  // the modal height calculation has to be changed since our custom template is
-  // taller than the default template
-  LightboxProvider.calculateModalDimensions = function (dimensions) {
-    var width = Math.max(400, dimensions.imageDisplayWidth + 32);
-
-    if (width >= dimensions.windowWidth - 20 || dimensions.windowWidth < 768) {
-      width = 'auto';
-    }
-
-    return {
-      'width': width,                             // default
-      'height': 'auto'                            // custom
-    };
-  };
-
-});
