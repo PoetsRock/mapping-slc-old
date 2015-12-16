64ed80fa35826608f12ddadbc51e064cff72477f
PoetsRock <chris@christanseer.com>
2015-12-16 00:05:59 -0700

    removed min js files

--- config/assets/production.js
+++ config/assets/production.js
@@ -8,18 +8,18 @@ module.exports = {
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
       ],
       js: [
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
+        'public/lib/angular/angular.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
       ]
     },
     css: 'public/dist/application.min.css',
-    js: 'public/dist/application.min.js'
+    js: 'public/dist/application.js'
   }
 };
