e084433e6766240d05a16074d7f776bf51fa7944
PoetsRock <chris@christanseer.com>
2015-12-18 01:18:43 -0700

    updated routes

--- modules/core/client/config/core-admin.client.routes.js
+++ modules/core/client/config/core-admin.client.routes.js
@@ -1,16 +1,28 @@
 //'use strict';
 //
 //// Setting up route
-//angular.module('core.admin.routes').config(['$stateProvider',
+//angular.module('core.menu').config(['$stateProvider',
 //  function ($stateProvider) {
 //    $stateProvider
-//      .state('admin', {
+//      .state('menu', {
 //        abstract: true,
-//        url: '/admin',
-//        template: '<ui-view/>',
-//        data: {
-//          roles: ['admin']
-//        }
-//      });
+//        url: '/menu',
+//        template: '<ui-view/>'
+//        //data: {
+//        //  roles: ['admin']
+//        //}
+//      })
+//    .state('settings', {
+//      //url: '/subscribe-form',
+//      templateUrl: 'modules/projects/client/directives/views/dropdown-list.html'
+//    })
+//    .state('uploads', {
+//      url: '/uploads',
+//      templateUrl: 'modules/core/views/file-upload.client.view.html'
+//    })
+//    .state('uploadFile', {
+//      url: '/uploads/:fileHash'
+//      //templateUrl: 'modules/users/views/create-user.client.view.html'
+//    });
 //  }
 //]);
