90db7700b6aadfdbce800be45dc3ff919c21e141
PoetsRock <chris@christanseer.com>
2015-12-23 19:56:34 -0700

    added route authentication based on user roles

--- modules/admins/client/config/admins.client.config.js
+++ modules/admins/client/config/admins.client.config.js
@@ -8,4 +8,19 @@ angular.module('admins').config(['$compileProvider',
     // https://docs.angularjs.org/guide/production
     $compileProvider.debugInfoEnabled(false);
   }
-]);
+])
+
+.run(function($rootScope, $state, Authentication) {
+  var $currentUser = Authentication.user;
+  $rootScope.$on('$stateChangeStart', function(e, to) {
+    if (!angular.isFunction(to.data)) return;
+    var result = to.data($currentUser);
+
+    if (result && result.to) {
+      e.preventDefault();
+      // Optionally set option.notify to false if you don't want
+      // to retrigger another $stateChangeStart event
+      $state.go(result.to, result.params, {notify: false});
+    }
+  });
+});
