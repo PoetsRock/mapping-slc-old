212f2a73604acc5f1a861f27a20ce948d125bd21
PoetsRock <chris@christanseer.com>
2015-12-24 07:16:41 -0700

    updating aws s3 image uploader and downloader & disabling unused files

--- modules/users/client/controllers/settings/change-profile-picture.client.controller.js
+++ modules/users/client/controllers/settings/change-profile-picture.client.controller.js
@@ -12,7 +12,87 @@ angular.module('users').controller('ChangeProfilePictureController', ['$scope', 
 
     $scope.user = Authentication.user;
     $scope.uploading = false;
+    $scope.imageURL = $scope.user.profileImageURL;
     var upload = null;
+
+
+    $scope.onFileSelect = function (files) {
+
+      if (files.length > 0) {
+        $scope.uploading = true;
+        var filename = files[0].name;
+        var type = files[0].type;
+        var query = {
+          user: $scope.user,
+          filename: filename,
+          type: type
+        };
+        var configObj = {cache: true};
+        $http.post('api/v1/s3/upload', query, configObj)
+          .success(function (result) {
+            console.log('result v1\n', result);
+            Upload.upload({
+              url: result.url, //s3Url
+              transformRequest: function (data, headersGetter) {
+                var headers = headersGetter();
+                delete headers.Authorization;
+                console.log('data v1\n', data);
+                return data;
+              },
+              fields: result.fields, //credentials
+              method: 'POST',
+              file: files[0]
+            }).progress(function (evt) {
+
+              console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total));
+
+            }).success(function (data, status, headers, config) {
+
+              var s3Result = xmlToJSON.parseString(data);   // parse
+
+              // file is uploaded successfully
+              $scope.uploading = false;
+              console.log('status: ', status);
+              console.log('The file ' + config.file.name + ' is uploaded successfully.\nResponse:\n', s3Result);
+
+              $scope.imageURL = 'https://s3-us-west-1.amazonaws.com/mapping-slc-file-upload/' + s3Result.PostResponse[0].Key[0]._text;
+
+              console.log('$scope.imageURL 111:\n', $scope.imageURL);
+
+
+            }).error(function () {
+
+            });
+
+
+            if (result && result.s3Url) {
+              console.log('result v3452\n', result);
+              $scope.user.profileImageURL = result.s3Url;
+              $scope.imageURL = result.s3Url;
+            }
+          })
+          .error(function (data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            $scope.uploading = false;
+          });
+
+        ////abort request
+        //  .xhr(function(xhr) {
+        //    $scope.abort = function() {
+        //    xhr.abort();
+        //    $scope.uploading = false;
+        //  };
+        //});
+
+      }
+
+
+    };
+
+
+
+
 
     /**
      *
@@ -21,7 +101,7 @@ angular.module('users').controller('ChangeProfilePictureController', ['$scope', 
      * @param files {object} a Blob that contains the file(s) to upload
      */
       //$scope.onFileSelect = function (files, requestType) {
-    $scope.onFileSelect = function (files) {
+    $scope.onFileSelectNEW = function (files) {
       //if (files.length > 0) {
       $scope.uploading = true;
 
