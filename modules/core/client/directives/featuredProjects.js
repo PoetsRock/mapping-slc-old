2b263b36569dba83cd5397a13f29055a10a6bed0
PoetsRock <chris@christanseer.com>
2016-01-11 20:11:42 -0700

    menu and featured projects

--- modules/core/client/directives/featuredProjects.js
+++ modules/core/client/directives/featuredProjects.js
@@ -6,7 +6,7 @@ angular.module('core').directive('featuredProjects', function () {
     templateUrl: '/modules/core/client/directives/views/featured-projects.html',
     controller: function ($scope, $http) {
       //provides logic for displaying featured project's title on mouseover
-      $scope.showTitle = false;
+      //$scope.showTitle = false;
       //$scope.showTitle = true;
     }
   };
