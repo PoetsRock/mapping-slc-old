88b3f1c910510613a7c8309f3e5f246481b021ef
PoetsRock <chris@christanseer.com>
2016-02-02 19:49:42 -0700

    deleted new relic

--- server.js
+++ server.js
@@ -3,24 +3,26 @@
 /**
  * Module dependencies.
  */
-console.log('process.env:\n', process.env);
-console.log('process.env.NODE_ENV:\n', process.env.NODE_ENV);
-if(process.env.NODE_ENV === 'production') {
-  //let NEW_RELIC_KEY = require('config/env/local-development.js');
-  require('newrelic');
-}
+
+//if(process.env.NODE_ENV === 'production') {
+//  let NEW_RELIC_KEY = require('config/env/local-development.js');
+  //require('newrelic');
+//}
+
 var app = require('./config/lib/app');
 var server = app.start();
+//console.log('process.env:\n', process.env);
+console.log('process.env.NODE_ENV:\n', process.env.NODE_ENV);
 
 console.log('   __    __                            __     __                                            __       __');
 console.log('  /  |  /  |                          /  |   /  |                                          /  \     /  |');
-console.log('  $$ |  $$ | ______  __    __        _$$ |_  $$ |____   ______   ______   ______           $$  \   /$$ | ______   ______   ______   ______   ______');
-console.log('  $$ |__$$ |/      \\/  |  /  |      / $$   | $$      \\ /      \\ /      \\ /      \\          $$$  \\ /$$$ |/      \\ /      \\ /      \\ /      \\ /\\');
+console.log('  $$ |  $$ | ______   __   __        _$$ |_  $$ |____    ______    ______    ______        $$  \   /$$ | ______    ______    ______    ______    ______');
+console.log('  $$ |__$$ |/      \\/ |  /  |      / $$   | $$      \\ /      \\ /      \\ /      \\      $$$ \\ /$$$ |/      \\ /      \\ /    \\ /     \\ /\\');
 console.log('  $$    $$ /$$$$$$  $$ |  $$ |      $$$$$$/  $$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$  |         $$$$  /$$$$ |$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$  |');
 console.log('  $$$$$$$$ $$    $$ $$ |  $$ |        $$ | __$$ |  $$ $$    $$ $$ |  $$/$$    $$ |         $$ $$ $$/$$ |/    $$ $$ |  $$ $$ |  $$ $$    $$ $$ |  $$/');
-console.log('  $$ |  $$ $$$$$$$$/$$ \\__$$ |        $$ |/  $$ |  $$ $$$$$$$$/$$ |     $$$$$$$$/ __       $$ |$$$/ $$ /$$$$$$$ $$ |__$$ $$ |__$$ $$$$$$$$/$$ |__');
+console.log('  $$ |  $$ $$$$$$$$/$$\\__$$ |        $$ |/  $$ |  $$ $$$$$$$$/$$ |     $$$$$$$$/  __      $$ |$$$/ $$ /$$$$$$$ $$ |__$$ $$ |__$$ $$$$$$$$/$$ |__');
 console.log('  $$ |  $$ $$       $$    $$ |        $$  $$/$$ |  $$ $$       $$ |     $$       /  |      $$ | $/  $$ $$    $$ $$    $$/$$    $$/$$       $$ /  |');
 console.log('  $$/   $$/ $$$$$$$/ $$$$$$$ |         $$$$/ $$/   $$/ $$$$$$$/$$/       $$$$$$$/$$/       $$/      $$/ $$$$$$$/$$$$$$$/ $$$$$$$/  $$$$$$$/$$/$$/');
-console.log('                    /  \\__$$ |                                                   $/                             $$ |     $$ |');
+console.log('                   /  \\__$$ |                                                   $/                             $$ |     $$ |');
 console.log('                    $$    $$/                                                                                   $$ |     $$ |');
 console.log('                     $$$$$$/                                                                                    $$/      $$/');
