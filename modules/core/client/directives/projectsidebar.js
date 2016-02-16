121471d67e29049fed49b5c1acb23d2f850d4b7d
PoetsRock <chris@christanseer.com>
2016-02-15 22:00:37 -0700

    sidebar animation

--- modules/core/client/directives/projectsidebar.js
+++ modules/core/client/directives/projectsidebar.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('core').directive('featureSideBar', function () {
+angular.module('core').directive('projectSideBar', function () {
   return {
     restrict: 'EA',
     templateUrl: '/modules/core/client/directives/views/projects-sidebar.html',
@@ -15,13 +15,20 @@ angular.module('core').directive('featureSideBar', function () {
         jQuery('.menu-ui').css('right','1.5%');
       };
 
+      if($scope.toggleSidebar) {
+        $scope.$on('MenuService.update', function () {
+          $scope.hideSidebar();
+        });
+      }
+
       $scope.showSidebar = function (markerId, projectData) {
         if (currentMarkerId !== markerId || currentMarkerId === markerId && !$scope.toggleSidebar) {
           $scope.project = projectData;
           $scope.toggleSidebar = true;
           currentMarkerId = markerId;
-          jQuery('.leaflet-top.leaflet-right').css('right','20%');
-          jQuery('.menu-ui').css('right','22%');
+          //jQuery('.leaflet-top.leaflet-right').css('right','25%');
+          //jQuery('.menu-ui').css('right','27%');
+
         } else {
           $scope.toggleSidebar = false;
           jQuery('.leaflet-top.leaflet-right').css('right','0');
